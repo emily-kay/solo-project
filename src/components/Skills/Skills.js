f83dec8c9a240f95a27f356fcd9f20f146dd8c27
Emily Kay <emilykay917@gmail.com>
2018-06-07 11:09:52 -0500

    Skills inputs styled to the right spot

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
-import Button from '@material-ui/core/Button';
+import { Button, Checkbox, Input } from '@material-ui/core';
 
 
 const mapStateToProps = state => ({
   user: state.user,
 });
 
-class InfoPage extends Component {
-  componentDidMount() {
-    this.props.dispatch({type: USER_ACTIONS.FETCH_USER});
+class SkillsPage extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      skills: [],
+      id: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+    axios({
+      method: 'GET',
+      url: '/api/skills'
+    }).then((response) => {
+      this.setState({
+        skills: response.data,
+      });
+    }).catch((error) => {
+      console.log('Error on the skills componentDidMount:', error);
+    });
   }
 
   componentDidUpdate() {
@@ -20,7 +40,11 @@ class InfoPage extends Component {
     }
   }
 
-  handleChange = (event) => {
+  handleChange = id => event => {
+    this.setState({ [id]: event.target.checked });
+  };
+
+  handleClick = (event) => {
     this.props.history.push('/powers');
   }
 
@@ -28,11 +52,25 @@ class InfoPage extends Component {
     return (
       <div >
         <h1>Strengths and Weaknesses</h1>
-        <Button onClick={this.handleChange}>Onward!</Button>
+        <p className="Directions">Here are some instructions! </p>
+        <ul className="skillsUL">{this.state.skills.map(data => {
+          return (
+            <li className="skillsLI" key={data.id}>
+              <Input
+                checked={this.state.checkedBox}
+                onChange={this.handleChange()}
+                value= 'number'
+              />
+              {data.skill}
+            </li>
+          );
+        })}
+        </ul>
+        <Button onClick={this.handleClick}>Onward!</Button>
       </div>
     );
   }
 }
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(InfoPage);
+export default connect(mapStateToProps)(SkillsPage);
